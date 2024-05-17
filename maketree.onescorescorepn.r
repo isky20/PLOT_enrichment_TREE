ebc64b1e1468f9c90a923fc58fcb39fd4df1f7bd
Ishak Yusuf <142504615+isky20@users.noreply.github.com>
2024-05-17 12:14:56 +0200

    Update maketree.onescorescorepn.r

--- maketree.onescorescorepn.r
+++ maketree.onescorescorepn.r
@@ -9,7 +9,7 @@ library(ggplot2)
 library(ggstar)
 
 
-df <- read.csv("lumB_lumA.csv", sep = ";")
+df <- read.csv("/home/cnr/Desktop/THEWORK2803/10.Amyloidosis(heart)_H.sapiens/data_for_tree/tabs_first5_coppi_chord/TTR_control.csv")
 all_genes <- unique(unlist(strsplit(gsub(" ", "", df$MergedGenes), ",")))
 # Create a binary matrix indicating gene presence for each term
 
@@ -28,7 +28,7 @@ dendrogram_object <- as.dendrogram(hclust_object)
 newick <- as.phylo(dendrogram_object)
 
 p1 <- ggtree(newick, branch.length="none", layout="circular", open.angle=10)+
-  geom_tiplab(align=TRUE, linesize = 0, size = 5, offset = 6) + 
+  geom_tiplab(align=TRUE, linesize = 0, size = 4, offset = 4) + 
   theme(plot.title = element_text(hjust = 0.5)) 
 
 
@@ -37,7 +37,7 @@ p2 <- p1+
   geom_fruit(
     data = df,
     geom = geom_col,
-    mapping = aes(y=description, x=score, fill = correlated.to), offset = 0.25,
+    mapping = aes(y=description, x=score, fill = correlated.to),
     axis.params=list(
       axis = "x"),
     grid.params=list()) + scale_fill_manual(values=c("#49FFFF","#F46D43")) 
@@ -56,8 +56,6 @@ p3 = p3 +
   )
 
 # Save the plot with a white background covering the whole plot area
-ggsave("lumB_lumA.svg", p3, device = "svg",
+ggsave("TTR_control.svg", p3, device = "svg",
        width = 8, height = 6, units = "in", dpi = 1200)
-
-
 
